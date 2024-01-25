f499dfabf171d9b486befa69d4635a99d60e6396
raphael-coeffic <156909216+raphael-coeffic@users.noreply.github.com>
2024-01-25 19:44:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -15,27 +15,27 @@ int main()
             printf(" %d", i);
     }
 
-    printf("\nThe Palindromes are:");
+    printf("The Palindromes are:");
     for (int i = a; i <= b; i++)
     {
         if (isPalindrome(i))
             printf(" %d", i);
     }
 
-    printf("\nThe Prime numbers are:");
+    printf("The Prime numbers are:");
     for (int i = a; i <= b; i++)
     {
         if (isPrime(i))
             printf(" %d", i);
     }
 
-    printf("\nThe Strong numbers are:");
+    printf("The Strong numbers are:");
     for (int i = a; i <= b; i++)
     {
         if (isStrong(i))
             printf(" %d", i);
     }
-    printf("\n");
+    // printf("\n");
 
     return 0;
 }
