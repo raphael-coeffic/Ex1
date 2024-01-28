ce81c5076aafb9fd884911e46a1fe08de6e6c1e3
raphael-coeffic <156909216+raphael-coeffic@users.noreply.github.com>
2024-01-28 15:32:20 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -4,10 +4,10 @@ int isPrime (int x)
 {
     if (x < 1) 
         return FALSE;
-
+    // Even numbers
     if (x != 2 && x % 2 == 0)
         return FALSE;
-
+    // check if we can divide the number by an odd number
     for (int i = 3; i < x; i = i + 2)
     {
         if (x % i == 0)
@@ -18,6 +18,7 @@ int isPrime (int x)
 
 }
 
+// This function give us the factorial of a number (Ex: 3! = 6)
 int factor (int x)
 {
     int ans = 1;
@@ -29,6 +30,7 @@ int factor (int x)
     
 }
 
+
 int isStrong (int x)
 {
     int sum = 0;
@@ -38,9 +40,12 @@ int isStrong (int x)
 
     while (help_num > 0)
     {
+        // take the next digit
         digit = help_num % 10;
+        // digit!
         factorial = factor (digit);
         help_num /= 10;
+        // add to the sum of the factorials of the digits
         sum += factorial;
     }
 
